c70c870de4bf50442a40350bafdbb1f6d49e4d60
石原 祐希 <issi0430bjc@gmail.com>
2024-07-31 20:42:40 +0900

    Vercel上でのbuild修正用

--- src/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server"
-import axios from "axios";
-
-export const GET = async (request: NextRequest): Promise<NextResponse> => {
-    const response = await axios.get(process.env.BACKEND_URL + "/api/dishes");
-    return NextResponse.json(response.data, { status: 200 })
-}
